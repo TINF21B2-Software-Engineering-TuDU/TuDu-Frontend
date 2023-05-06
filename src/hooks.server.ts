694a0c6fddb79fa95a03b2565d676377c4a48510
DE38 <niklas@gt-trenz.de>
2023-05-06 19:20:55 +0200

    prepare for JWTs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,8 @@
 import type { Handle, HandleFetch } from '@sveltejs/kit';
+import {PUBLIC_API_URL} from "$env/static/public";
 
 export const handleFetch = (async ({ event, request, fetch }) => {
-	if (request.url.startsWith('http://localhost:8080')) {
+	if (request.url.startsWith(PUBLIC_API_URL)) {
 		request.headers.set('auth_token', event.request.headers.get("cookie").split(' ')[0].slice(11).slice(0, -1));
 		request.headers.append('email', event.locals.user.username);
 	}
@@ -28,9 +29,16 @@ const getUser = async (auth_token: string) => {
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
+	const user = event.locals.user;
+
+	if (user) { // user exists: go on
+		return await resolve(event);
+	}
+
 	// read session cookie
-	const session = event.cookies.get('auth_token');
-	if (!session) {
+	const session = event.cookies.get('AuthorizationToken');
+
+	if (!session) { // no session: user needs to login
 		event.locals.user = {
 			username: "User",
 			isLoggedIn: false,
@@ -39,10 +47,10 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return await resolve(event);
 	}
 
-	// get user info using session cookie
+	// no user: get user information from database
 	const response = await getUser(session);
 
-	// DB error 
+	// database error
 	if (response.response.status >= 400 || !response.response.ok) {
 		event.locals.user = {
 			username: "User",
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,12 +1,15 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Action, Actions, PageServerLoad } from './$types';
 import bcrypt from 'bcrypt';
+import {PUBLIC_API_URL} from "$env/static/public";
+import jwt, {JsonWebTokenError} from "jsonwebtoken";
+import * as jose from 'jose';
 
 export const load: PageServerLoad = async () => {
 };
 
 const postLogin = async (username: string, password: string) => {
-	const response = await fetch('http://localhost:8080/auth/v1/login', {
+	const response = await fetch(PUBLIC_API_URL + '/auth/v1/login', {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json',
@@ -32,40 +35,30 @@ const login: Action = async ({ cookies, request }) => {
 		return fail(400, { invalid: true });
 	}
 
-	// hier user aus DB anfordern
-	const jsonData = await postLogin(username, password);
+	// request JWT from database
+	const response = await postLogin(username, password);
 
 	// error-handling
-	// if user already exists
-	// other error
-	if (jsonData.response.status >= 400) {
-		return fail(400, { credentials: true });
+	if (response.response.status >= 400 || !response.response.ok) {
+		return fail(response.response.status, { credentials: true });
 	}
 
-	if (jsonData.response.headers.get('set-cookie') == null) {
-		return fail(400, { credentials: true });
-	}
+	const {token: authentication_token} = response.json;
 
-	const tokens = jsonData.response.headers.get('set-cookie').split(' ');
-	const auth_token = tokens[0].slice(11).slice(0, -1);
-	const refresh_token = tokens[2].slice(14).slice(0, -1);
+	console.log(response.response);
+	console.log(response.json);
+	console.log(jwt.decode(authentication_token))
+	console.log(jose.decodeProtectedHeader(authentication_token))
+	console.log(jose.decodeJwt(authentication_token))
+	console.log(jose.base64url)
 
-	cookies.set('auth_token', auth_token, {
-		path: '/',
+	// Set JWT as cookie
+	cookies.set('AuthorizationToken', authentication_token, {
 		httpOnly: true,
-		sameSite: 'strict',
-		secure: process.env.NODE_ENV === 'production',
-		maxAge: 60 * 60 * 24 * 30
-		// domain: '127.0.0.1'
-	});
-
-	cookies.set('refresh_token', refresh_token, {
 		path: '/',
-		httpOnly: true,
+		secure: true,
 		sameSite: 'strict',
-		secure: process.env.NODE_ENV === 'production',
-		maxAge: 60 * 60 * 24 * 30
-		// domain: '127.0.0.1'
+		maxAge: 60 * 60 * 24 * 30 // = 30 days
 	});
 
 	// redirect the user
