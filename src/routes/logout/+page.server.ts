3d4da72d5bc087dfe65c352dc795a54a9c23d9c8
DE38 <niklas@gt-trenz.de>
2023-05-07 12:50:07 +0200

    rework logout still work necessary

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,38 +1,49 @@
-import { fail, redirect } from '@sveltejs/kit';
-import type { Actions, PageServerLoad } from './$types';
+import {fail, redirect} from '@sveltejs/kit';
+import type {Actions, PageServerLoad} from './$types';
+import {PUBLIC_API_URL} from "$env/static/public";
 
 export const load: PageServerLoad = async () => {
-	// logout is only serverside and deteles cookies and informs API
-	// user does not need to see this page
-	throw redirect(302, '/');
+    // logout is only serverside and deletes cookies and informs API
+    // user does not need to see this page
+    throw redirect(302, '/');
 };
 
-const getLogout = async (username?: string) => {
-	const response = await fetch('http://localhost:8080/api/v1/logout', {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json'
-		},
-		credentials: 'same-origin',
-		body: JSON.stringify({ email: username })
-	});
-	return response;
+const getLogout = async (auth_token?: string) => {
+    const response = await fetch(PUBLIC_API_URL + '/api/v1/logout', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify({token: auth_token})
+    });
+    return response;
 }
 
 export const actions: Actions = {
-	async default({ cookies, request }) {
-		// TODO: change form to load function from the hook
-		if (!username)
-			throw redirect(302, "/main");
+    async default({cookies, locals}) {
+        console.log("User Logout")
+        const auth_token = JSON.stringify({token: cookies.get('AuthorizationToken')})
 
-		const logout = await getLogout(username);
-		if (logout.status >= 400)  return redirect
+        const logout = await getLogout(auth_token);
+        if (logout.status >= 400) {
+            // display error message in separate page
+            return redirect(302, '/main')
+        }
 
-		// eat the cookie
-		await cookies.delete('auth_token');
-		await cookies.delete('refresh_token');
+        // reset user data
+        locals.user = {
+            username: "User",
+            isLoggedIn: false,
+            loginTime: new Date(),
+        };
 
-		throw redirect(302, '/login');
-	}
+        console.log(locals.user)
+
+        // eat the cookie
+        await cookies.delete('AuthorizationToken');
+
+        throw redirect(302, '/login');
+    }
 };
