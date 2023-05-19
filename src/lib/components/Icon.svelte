47720c438eb670c56722f81d1c5ac1b1039c4a9d
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2023-05-19 15:17:47 +0200

    Icons

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,18 @@
+<script>
+	let name = '';
+    export { name };
+</script>
+
+<style>
+    .icon {
+        width: 30px;
+        height: 30px;
+    }
+</style>
+
+{#if name === ''}
+	<!-- error image -->
+	<img class="icon" src="https://i.imgur.com/8Lg5ZtJ.png" alt="error" />
+{:else}
+    <img class="icon" src="/icons/{name}.png" alt="{name}">
+{/if}
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,7 @@
+<!-- import icon from lib -->
+<script>
+    import Icon from '$lib/components/Icon.svelte';
+</script>
+
+<!-- ICON -->
+<Icon name="RECURRENCE"/>
