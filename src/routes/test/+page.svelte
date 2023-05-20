0c37f2ac0d1b178f08af4171464acf7e770f58f2
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2023-05-20 13:59:14 +0200

    Cleanup

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,28 +1,32 @@
 <!-- import icon from lib -->
 <script>
-    import Icon from '$lib/components/Icon.svelte';
-    import Checkbox from '$lib/components/Checkbox.svelte';
-    import Logo from '$lib/components/Logo.svelte';
-    import Divider from '$lib/components/Divider.svelte';
+	import Icon from '$lib/components/Icon.svelte';
+	import Checkbox from '$lib/components/Checkbox.svelte';
+	import Logo from '$lib/components/Logo.svelte';
+	import Divider from '$lib/components/Divider.svelte';
 </script>
 
 <!-- ICON -->
-<Icon name="RECURRENCE"/>
+<Icon name="RECURRENCE" />
 
 <!-- Checkboxes -->
 <div style="display: flex; flex-direction: row;">
-    <Checkbox checked/>
-    <Checkbox/>
+	<Checkbox checked />
+	<Checkbox />
 </div>
 
 <!-- Logo -->
 <div style="display: flex; flex-direction: column;">
-    <Logo/>
-    <Logo integrated={false}/>
-    <Logo label={false}/>
-    <Logo label={false} integrated={false}/>
+	<div style="display: flex; flex-direction: row; align-items: baseline; gap: 10px;">
+		<Logo />
+		<Logo label={false} />
+	</div>
+	<div style="display: flex; flex-direction: row; align-items: baseline; gap: 10px;">
+		<Logo integrated={false} />
+		<Logo label={false} integrated={false} />
+	</div>
 </div>
 
 <!-- Divider -->
-<Divider/>
-<Divider/>
+<Divider />
+<Divider />
