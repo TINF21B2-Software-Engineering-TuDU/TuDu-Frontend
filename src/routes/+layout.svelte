50e92281d4d70743afcb8ca41fcaeda01d52cfca
Proliecan <65495509+Proliecan@users.noreply.github.com>
2023-05-25 17:30:47 +0200

    make page fill etire screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,29 +5,38 @@
 	import './style.css';
 </script>
 
-<div class="app">
-	<Header />
+<div class="page">
+	<div class="background" id="bg-layer-1" />
+	<div class="background" id="bg-layer-2" />
 
-	<div class="page">
-		<div class="background" id="bg-layer-1" />
-		<div class="background" id="bg-layer-2" />
+	<Icon
+		name="SIDEBAR-EXPAND"
+		width="30px"
+		interactive
+		onclick={() => {
+			alert('//todo:Expand sidebar');
+		}}
+	/>
 
-		<Icon name="SIDEBAR-EXPAND" width="30px" interactive 
-				onclick={()=>{
-					alert("//todo:Expand sidebar");
-				}} />
-
-		<main><slot /></main>
-	</div>
-
-	<Footer />
+	<main><slot /></main>
 </div>
 
 <style>
-	.app {
+	.page {
 		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
+		flex-direction: row;
+		flex: 1;
+
+		/* Fix screen exactly without scrolling */
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+
+		margin: none;
+		padding: 20px;
+		gap: 40px;
 	}
 
 	main {
@@ -39,21 +48,9 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
 
-	.page {
-		position: relative;
-
-		display: flex;
-		flex-direction: row;
-		flex: 1;
-
-		margin: 0 auto;
-		width: 100%;
-		
-		align-items: top;
-		padding:20px;
-		gap: 40px;
+		height: 100%;
+		overflow: auto;
 	}
 
 	.background {
@@ -65,6 +62,8 @@
 
 		z-index: -1;
 		border-radius: 10px 0 0 0;
+
+		border: 1px solid yellow;
 	}
 
 	#bg-layer-1 {
@@ -72,12 +71,14 @@
 		background: #eeeeee;
 		opacity: 0.3;
 		border-radius: 10px 0px 0px 0px;
+		border: 1px solid blue;
 	}
 
 	#bg-layer-2 {
 		/* Marker */
-		background: linear-gradient(180deg, #18CDCA 70%, #8ce8e7 100%);
+		background: linear-gradient(180deg, #18cdca 70%, #8ce8e7 100%);
 		opacity: 0.3;
 		border-radius: 10px 0px 0px 0px;
+		border: 1px solid orange;
 	}
 </style>
